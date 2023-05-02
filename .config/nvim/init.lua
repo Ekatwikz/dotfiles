a86dcc150941ad2f8034f8b4251350a992cfe54a
Ekatwikz <ekatwikz@gmail.com>
2023-05-02 12:12:05 +0000

    removed k->gk stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -378,8 +378,8 @@ vim.o.timeoutlen = 300
 -- See `:help vim.keymap.set()`
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+--vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+--vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- [[ Undotree ]]
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = '[U]ndotree Toggle' })
